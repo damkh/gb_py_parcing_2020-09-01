b1d4b165351c43e0c64fbf8cf5b9ad1f33b63164
Damir <18482283+damkh@users.noreply.github.com>
2020-09-18 08:15:22 +0300

    les_5_task_2.py

--- les_5/dz/les_5_task_2.py
+++ les_5/dz/les_5_task_2.py
@@ -4,34 +4,46 @@
 """
 from selenium import webdriver
 import time
-from datetime import date
 from pymongo import MongoClient
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Firefox('/opt/Webdriver')
-driver.get("https://www.mvideo.ru/")
+main_link = "https://www.mvideo.ru/"
+driver.get(main_link)
 
-#
-# while True:
+# Инициализация MongoDB
+client = MongoClient('127.0.0.1', 27017)
+db = client['mvideo_ru']
+hits = db.hits
+
 time.sleep(3)
-# button = driver.find_element_by_xpath(
-#     "//div[@class='gallery-layout sel-hits-block ']/div/div/div/div/a[@class='next-btn sel-hits-button-next']"
-# )
+# hit_items = []
 while True:
     try:
-        button = WebDriverWait(driver,10).until(
+        button = WebDriverWait(driver, 10).until(
                     EC.presence_of_element_located((By.XPATH,
-                                                    "//div[@class='gallery-layout sel-hits-block ']/div/div/div/div/a[@class='next-btn sel-hits-button-next']"))
+                                                    "(//div[@class='gallery-layout sel-hits-block '])"
+                                                    "[2]/div/div/div/div/a[@class='next-btn sel-hits-button-next']"))
                 )
         button.click()
+        time.sleep(3)
     except:
         print('OUT')
+        hit_block = driver.find_elements_by_xpath("(//div[@class='gallery-layout sel-hits-block '])[2]")
+        items_a = hit_block[0].find_elements_by_xpath(
+            ".//ul/li[@class='gallery-list-item height-ready']/div/div/div/a"
+        )
+        for item in items_a:
+            item_link = item.get_attribute('href')
+            data_dict = eval(item.get_attribute('data-product-info').replace('\t', '').replace('\n', ''))
+            hit_item_dict = {
+                'Product_name': data_dict['productName'],
+                'Product_category': data_dict['productCategoryName'],
+                'Product_price': float(data_dict['productPriceLocal']),
+                'Product_link': item_link
+            }
+            # hit_items.append(hit_item_dict)
+            hits.insert_one(hit_item_dict)
         break
-
-# next-btn sel-hits-button-next disabled
-# //div[@class='gallery-layout sel-hits-block ']/div/div/div/div/div/ul/li[@class='gallery-list-item height-ready']
-time.sleep(3)
-# button.click()
-print()
